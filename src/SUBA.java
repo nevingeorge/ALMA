ce6e5dc7a2ff0959ac2000dff970cc593f94b8b5
Nevin George <nevin.george@yale.edu>
2020-04-12 20:55:25 -0500

    Added parameters for the SUBA

--- src/SUBA.java
+++ src/SUBA.java
@@ -26,16 +26,29 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class SUBA {
+	// SUBA = (Q, Σ, ∆, F)
+	
+	// Q has states q_1 to q_(Q_SUBA)
+	public static int Q_SUBA;
+	
+	// transition matrix Δ
+	public static boolean[][][] transition_SUBA;
+	
+	// final states F
+	public static boolean[] F_SUBA;
+		
+	// --------------------------------------
+	
 	// UFA = (Q',ΣU{$},∆',F')
 	
-	// Q' has states q_1 to q_numStates
-	public static int numStates;
+	// Q' has states q_1 to q_(Q_UFA)
+	public static int Q_UFA;
 	
 	// transition matrix Δ'
-	public static boolean[][][] transition;
+	public static boolean[][][] transition_UFA;
 	
 	// final states F'
-	public static boolean[] F;
+	public static boolean[] F_UFA;
 
 	public static void main(String[] args) throws Exception {
 		// converts the input SUBA into an equivalent UFA
@@ -46,6 +59,9 @@ public class SUBA {
 		
 		// runs Mod2_MA.java on the mod-2-MA
 		Mod2_MA.run();
+		
+		if(!finalCheck())
+			throw new Exception("Failed final check");
 	}
 	
 	public static void SUBAtoUFA() throws Exception {
@@ -56,8 +72,7 @@ public class SUBA {
 		 * <characters in the alphabet>
 		 * <final states (F)>
 		 * <number of transitions>
-		 * The remaining lines are the transitions, with each line having the form q_j a q_k, where q_j,q_k∈Q
-		 * and a∈Σ.
+		 * The remaining lines are the transitions, with each line having the form q_i a q_j, where q_i,q_j∈Q and a∈Σ.
 		 * 
 		 * By default the only initial state of the SUBA (and therefore also the UFA) is q_1.
 		 * Example input files can be found in the repository.
@@ -76,13 +91,14 @@ public class SUBA {
 		BufferedReader f = new BufferedReader(new FileReader(arrInput[0]));
 		System.out.println("");
 
+		// number of states
 		// Q' = Q U (Q x Q x {0,1})
 		String line = f.readLine();
 		while(line.charAt(0) == '/' && line.charAt(1) == '/')
 			line = f.readLine();
 		
-		int Q = Integer.parseInt(line);
-		numStates = Q + Q*Q*2;
+		Q_SUBA = Integer.parseInt(line);
+		Q_UFA = Q_SUBA + Q_SUBA*Q_SUBA*2;
 		
 		// --------------------------------------------------------------------------------------
 		
@@ -123,23 +139,22 @@ public class SUBA {
 		
 		// --------------------------------------------------------------------------------------
 		
-		// final states for input SUBA
+		// final states for the SUBA
 		line = f.readLine();
 		while(line.charAt(0) == '/' && line.charAt(1) == '/')
 			line = f.readLine();
 		st = new StringTokenizer(line);
 		
-		boolean[] F_SUBA = new boolean[Q+1];
+		F_SUBA = new boolean[Q_SUBA+1];
 		while(st.hasMoreTokens()) {
 			int state = Integer.parseInt(st.nextToken());
-		if(1<=state && state<=Q && !F_SUBA[state])
+			if(1<=state && state<=Q_SUBA && !F_SUBA[state])
 				F_SUBA[state] = true;
 			else {
 				f.close();
 				throw new Exception("Invalid input: invalid or duplicate final state");
 			}
 		}
-		
 		// --------------------------------------------------------------------------------------
 		
 		/* Following the paper by Bousquet and Löding, Δ' contains (where q,p,p'∈Q)
@@ -148,11 +163,10 @@ public class SUBA {
 		 * - all transitions of the form ((q,p,i),a,(q,p',i')), where (p,a,p')∈Δ, and
 		 * 	 i' = 1 if p'∈F and i if p'∉F
 		 * 
-		 * Transitions will be stored in a numStates x alphabetSize x numStates adjacency matrix.
-		 * Let the number of states in the SUBA be Q.
-		 * The first Q states of Δ' will be the states of the SUBA.
+		 * Transitions will be stored in a Q_UFA x alphabetSize x Q_UFA adjacency matrix.
+		 * The first Q_SUBA states of Δ' will be the states of the SUBA.
 		 * The remaining states will be of the form (q_j,q_k,i), where q_j,q_k∈Q and i∈{0,1}.
-		 * State (q_j,q_k,i) will be found at index (2*Q*j)+(2*k)-(Q)+(i-1) of Δ'.
+		 * State (q_j,q_k,i) will be found at index (2*Q_SUBA*j)+(2*k)-(Q_SUBA)+(i-1) of Δ'.
 		*/
 		
 		// number of transitions
@@ -161,13 +175,14 @@ public class SUBA {
 			line = f.readLine();
 
 		int numTransitions = Integer.parseInt(line);
-		if(numTransitions<1 || numTransitions>((Mod2_MA.alphabetSize-1)*Q*Q)) {
+		if(numTransitions<1 || numTransitions>((Mod2_MA.alphabetSize-1)*Q_SUBA*Q_SUBA)) {
 			f.close();
 			throw new Exception("Invalid input: invalid number of transitions");
 		}
 		
-		// transition matrix Δ' = (start state, letter, end state) 
-		transition = new boolean[numStates+1][Mod2_MA.alphabetSize][numStates+1];
+		// transition matrices Δ and Δ': (start state, letter, end state)
+		transition_SUBA = new boolean[Q_SUBA+1][Mod2_MA.alphabetSize][Q_SUBA+1];
+		transition_UFA = new boolean[Q_UFA+1][Mod2_MA.alphabetSize][Q_UFA+1];
 		
 		// lines of the form q_j a q_k, where q_j,q_k∈Q
 		for(int i=0;i<numTransitions;i++) {
@@ -187,43 +202,44 @@ public class SUBA {
 			
 			int p_end = Integer.parseInt(st.nextToken());
 			
-			if(p_start<1 || p_start>Q || p_end<1 || p_end>Q) {
+			if(p_start<1 || p_start>Q_SUBA || p_end<1 || p_end>Q_SUBA) {
 				f.close();
 				throw new Exception("Invalid input: invalid transition");
 			}
 			
-			// transition from Δ
-			transition[p_start][a][p_end] = true;
+			// Δ ⊆ Δ' 
+			transition_SUBA[p_start][a][p_end] = true;
+			transition_UFA[p_start][a][p_end] = true;
 			
 			// transitions of the form ((q,p,i),a,(q,p',i'))
 			// p'∈F so i'=1
 			if(F_SUBA[p_end]) {
-				for(int q=1;q<=Q;q++) {
+				for(int q=1;q<=Q_SUBA;q++) {
 					// ((q,p,0),a,(q,p',1))
-					transition[2*Q*q+2*p_start-Q-1][a][2*Q*q+2*p_end-Q] = true;
+					transition_UFA[2*Q_SUBA*q+2*p_start-Q_SUBA-1][a][2*Q_SUBA*q+2*p_end-Q_SUBA] = true;
 					// ((q,p,1),a,(q,p',1))
-					transition[2*Q*q+2*p_start-Q][a][2*Q*q+2*p_end-Q] = true;
+					transition_UFA[2*Q_SUBA*q+2*p_start-Q_SUBA][a][2*Q_SUBA*q+2*p_end-Q_SUBA] = true;
 				}
 			}
 			// p'∉F so i'=i
 			else {
-				for(int q=1;q<=Q;q++) {
+				for(int q=1;q<=Q_SUBA;q++) {
 					// ((q,p,0),a,(q,p',0))
-					transition[2*Q*q+2*p_start-Q-1][a][2*Q*q+2*p_end-Q-1] = true;
+					transition_UFA[2*Q_SUBA*q+2*p_start-Q_SUBA-1][a][2*Q_SUBA*q+2*p_end-Q_SUBA-1] = true;
 					// ((q,p,1),a,(q,p',1))
-					transition[2*Q*q+2*p_start-Q][a][2*Q*q+2*p_end-Q] = true;
+					transition_UFA[2*Q_SUBA*q+2*p_start-Q_SUBA][a][2*Q_SUBA*q+2*p_end-Q_SUBA] = true;
 				}
 			}
 		}
 
 		// --------------------------------------------------------------------------------------
 		
-		// transitions of the form (q,$,(q,q,0)), where q∈Q
-		// final states for UFA of the form (q,q,1), where q∈Q
-		F = new boolean[numStates+1];
-		for(int q=1;q<=Q;q++) {
-			transition[q][Mod2_MA.letterToIndex.get('$')][2*Q*q+2*q-Q-1] = true;
-			F[2*Q*q+2*q-Q] = true;
+		// transitions for the UFA of the form (q,$,(q,q,0)), where q∈Q
+		// final states for the UFA of the form (q,q,1), where q∈Q
+		F_UFA = new boolean[Q_UFA+1];
+		for(int q=1;q<=Q_SUBA;q++) {
+			transition_UFA[q][Mod2_MA.letterToIndex.get('$')][2*Q_SUBA*q+2*q-Q_SUBA-1] = true;
+			F_UFA[2*Q_SUBA*q+2*q-Q_SUBA] = true;
 		}
 		
 		line = f.readLine();
@@ -239,12 +255,12 @@ public class SUBA {
 
 	public static void UFAtoMod2MA() {
 		// size of the target function equals the number of states in the UFA
-		Mod2_MA.r = numStates;
+		Mod2_MA.r = Q_UFA;
 		
 		// fy is the characteristic vector of F
 		Mod2_MA.fy = new int[Mod2_MA.r];
-		for(int i=1;i<=numStates;i++) {
-			if(F[i])
+		for(int i=1;i<=Q_UFA;i++) {
+			if(F_UFA[i])
 				Mod2_MA.fy[i-1] = 1;
 		}
 		
@@ -253,7 +269,7 @@ public class SUBA {
 		for(int i=0;i<Mod2_MA.alphabetSize;i++) {
 			for(int j=1;j<=Mod2_MA.r;j++) {
 				for(int k=1;k<=Mod2_MA.r;k++) {
-					if(transition[j][i][k])
+					if(transition_UFA[j][i][k])
 						Mod2_MA.fu[i][j-1][k-1] = 1;
 				}
 			}
@@ -266,5 +282,9 @@ public class SUBA {
 		
 		// initial size of X and Y
 		Mod2_MA.l = 1;
+	}
+	
+	public static boolean finalCheck() {
+		return true;
 	}
 }
