f63da26205f958bb330d7e0adb9726375ff3f1b8
Nevin George <nevin.george@yale.edu>
2021-01-23 13:06:46 -0600

    Add standard error sta statistic.

--- Java Algorithms/src/statistics.java
+++ Java Algorithms/src/statistics.java
@@ -15,11 +15,8 @@ public class statistics {
 	
 	public static boolean inDFA;
 	
-	public static int[][] meanResultsM2MA;
-	public static int[][] meanResultsDFA;
-	
-	public static ArrayList<Integer>[] medianResultsM2MA;
-	public static ArrayList<Integer>[] medianResultsDFA;
+	public static ArrayList<Integer>[] resultsM2MA;
+	public static ArrayList<Integer>[] resultsDFA;
 
 	public static void main(String[] args) throws Exception {
 		Scanner in = new Scanner(System.in);
@@ -32,83 +29,44 @@ public class statistics {
 			System.out.println("Input file name.");
 			fileNames[i] = in.nextLine();
 		}
-		
-		inDFA = false;
-		
-		BufferedReader firstFile = new BufferedReader(new FileReader(fileNames[0]));
-		if (firstFile.readLine().contains("DFA")) {
-			inDFA = true;
-		}
-		firstFile.close();
-		
-		System.out.println("Enter 1 to give each file equal weighting or 2 to combine "
-				+ "the input from all the text files into one larger text file before calculating the statistics.");
-		int equalWeighting = Integer.parseInt(in.nextLine());
-	
 		in.close();
 		
-		if (equalWeighting == 1) {
-			// int[][][0] = mean results, int[][][1] = median results
-			int[][][] fileResultsM2MA = new int[numFiles][31][2];
-			int[][][] fileResultsDFA = new int[numFiles][31][2];
-			
-			for (int i = 0; i < numFiles; i++) {
-				initialize();
-				
-				readFile(fileNames[i]);
-				
-				for (int j = 1; j <= 30; j++) {
-					if (meanResultsM2MA[j][0] > 0) {
-						fileResultsM2MA[i][j][0] = calculateMean(meanResultsM2MA[j]);
-						fileResultsM2MA[i][j][1] = calculateMedian(medianResultsM2MA[j]);
-					}
-					
-					if (inDFA && meanResultsDFA[j][0] > 0) {
-						fileResultsDFA[i][j][0] = calculateMean(meanResultsDFA[j]);
-						fileResultsDFA[i][j][1] = calculateMedian(medianResultsDFA[j]);
-					}
-				}
-			}
-			
-			System.out.println("\nExperiment Results");
-			System.out.println("------------------");
-			
-			displayResultsEqualWeighting(numFiles, fileResultsM2MA, "M2MA");
-			
-			if (inDFA) {
-				displayResultsEqualWeighting(numFiles, fileResultsDFA, "DFA");
-			}
-		} else {
-			initialize();
-			
-			for (int i = 0; i < numFiles; i++) {
-				readFile(fileNames[i]);
-			}
-			
-			System.out.println("\nExperiment Results");
-			System.out.println("------------------");
-			
-			displayResultsCombined(meanResultsM2MA, medianResultsM2MA, "M2MA");
-			
-			if (inDFA) {
-				displayResultsCombined(meanResultsDFA, medianResultsDFA, "DFA");
-			}
+		inDFA = containsDFAResults(fileNames[0]);
+		
+		initialize();
+		
+		for (int i = 0; i < numFiles; i++) {
+			readFile(fileNames[i]);
 		}
+		
+		displayResults(resultsM2MA, "M2MA");
+		
+		if (inDFA) {
+			displayResults(resultsDFA, "DFA");
+		}
+	}
+	
+	public static boolean containsDFAResults(String firstFileName) throws IOException {
+		BufferedReader firstFile = new BufferedReader(new FileReader(firstFileName));
+		String line = firstFile.readLine();
+		firstFile.close();
+		
+		if (line.contains("DFA")) {
+			return true;
+		}
+		return false;
 	}
 	
 	@SuppressWarnings("unchecked")
 	public static void initialize() {
-		meanResultsM2MA = new int[31][2];
-		meanResultsDFA = new int[31][2];
-		
-		medianResultsM2MA = new ArrayList[31];
+		resultsM2MA = new ArrayList[31];
 		for (int i = 1; i <= 30; i++) {
-			medianResultsM2MA[i] = new ArrayList<Integer>();
+			resultsM2MA[i] = new ArrayList<Integer>();
 		}
 		
-		medianResultsDFA = new ArrayList[31];
+		resultsDFA = new ArrayList[31];
 		for (int i = 1; i <= 30; i++) {
-			medianResultsDFA[i] = new ArrayList<Integer>();
+			resultsDFA[i] = new ArrayList<Integer>();
 		}
 	}
 	
@@ -124,15 +82,11 @@ public class statistics {
 				int initialSize = Integer.parseInt(lineArr[0]);
 						
 				int convertedM2MASize = Integer.parseInt(lineArr[1]);
-				meanResultsM2MA[initialSize][0]++;
-				meanResultsM2MA[initialSize][1] += convertedM2MASize;
-				medianResultsM2MA[initialSize].add(convertedM2MASize);
+				resultsM2MA[initialSize].add(convertedM2MASize);
 				
 				if (inDFA) {
 					int convertedDFASize = Integer.parseInt(lineArr[2]);
-					meanResultsDFA[initialSize][0]++;
-					meanResultsDFA[initialSize][1] += convertedDFASize;
-					medianResultsDFA[initialSize].add(convertedDFASize);
+					resultsDFA[initialSize].add(convertedDFASize);
 				}
 				
 				line = f.readLine();
@@ -146,61 +100,55 @@ public class statistics {
 		f.close();
 	}
 	
-	public static void displayResultsEqualWeighting(int numFiles, int[][][] fileResults, String convertedAutomataName) {
-		System.out.println("\nInitial automata size: mean size of converted " + convertedAutomataName + ", median size of converted " + convertedAutomataName);
-		for (int j = 1; j <= 30; j++) {
-			int countMean = 0;
-			int sumMean = 0;
-			int countMedian = 0;
-			int sumMedian = 0;
-			
-			for (int i = 0; i < numFiles; i++) {
-				if (fileResults[i][j][0] > 0) {
-					countMean++;
-					sumMean += fileResults[i][j][0];
-				}
-				
-				if (fileResults[i][j][1] > 0) {
-					countMedian++;
-					sumMedian += fileResults[i][j][1];
-				}
-			}
-			
-			if (countMean > 0) {
-				int mean = (int) Math.round(sumMean / (double) countMean);
-				int median = (int) Math.round(sumMedian / (double) countMedian);
-				System.out.println(j + ": " + mean + ", " + median);
-			}
-		}
-	}
-	
-	public static void displayResultsCombined(int[][] meanResults, ArrayList<Integer>[] medianResults, String convertedAutomataName) {
-		System.out.println("\nInitial automata size: mean size of converted " + convertedAutomataName + ", median size of converted " + convertedAutomataName);
+	public static void displayResults(ArrayList<Integer>[] results, String convertedAutomataName) {
+		System.out.println("\n" + convertedAutomataName + " Results");
+		System.out.println("------------");
+		System.out.println("Initial automata size: mean, median, standard error");
 		for (int i = 1; i <= 30; i++) {
-			if (meanResults[i][0] > 0) {
-				int mean = calculateMean(meanResults[i]);
-				int median = calculateMedian(medianResults[i]);
+			if (results[i].size() > 0) {
+				double mean = calculateMean(results[i]);
+				double median = calculateMedian(results[i]);
+				double stdError = calculateStandardError(results[i], mean);
 				
-				System.out.println(i + ": " + mean + ", " + median);
+				System.out.println(i + ": " + mean + ", " + median + ", " + stdError);
 			}
 		}
 	}
 	
 	
-	public static int calculateMean(int[] arr) {
-		return (int) Math.round(arr[1] / (double) arr[0]);
+	public static double calculateMean(ArrayList<Integer> arr) {
+		int count = 0;
+		double sum = 0;
+		
+		for (int n : arr) {
+			count++;
+			sum += n;
+		}
+		
+		return Math.round((sum / count) * 100) / 100.0;
 	}
 
-	public static int calculateMedian(ArrayList<Integer> arr) {
+	public static double calculateMedian(ArrayList<Integer> arr) {
 		Collections.sort(arr);
 		
 		int len = arr.size();
 		
 		if (len % 2 == 0) {
-			return (int) Math.round((arr.get((len / 2) - 1) + arr.get(len / 2)) / 2.0);
+			return (arr.get((len / 2) - 1) + arr.get(len / 2)) / 2.0;
 		} else {
 			return arr.get(len / 2);
 		}
 	}
-
+	
+	public static double calculateStandardError(ArrayList<Integer> arr, double mean) {
+		int count = 0;
+		double sum = 0;
+		
+		for (int n : arr) {
+			count++;
+			sum += Math.pow(n - mean, 2);
+		}
+		
+		return Math.sqrt(sum / (count - 1)) / Math.sqrt(count);
+	}
 }
